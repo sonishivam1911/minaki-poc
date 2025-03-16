6c72ddd6814413d1fa62f01a1362b01810025868
ssoni-harmoni <ssoni@harmoni.io>
2025-03-16 09:08:34 -0400

    Sales order is being booked via ship rocket. Some issue while creating the sr forward function

--- utils/bhavvam/shiprocket.py
+++ utils/bhavvam/shiprocket.py
@@ -3,6 +3,7 @@ import json
 import os
 import pandas as pd
 from dotenv import load_dotenv
+from config.logger import logger
 
 SR_EMAIL = os.getenv("SR_EMAIL")
 SR_PASSWORD = os.getenv("SR_PASSWORD")
@@ -32,8 +33,44 @@ def check_service(token, pickup_pincode, delivery_pincode, weight):
     response = requests.request("GET", url, headers=headers, data=payload)
     return response.json()
 
-def create_sr_forward(token, order_data, length, breadth, height, weight,courier_id, pickup_location = "warehouse", ewaybill_no = None, request_pickup = True): 
+def create_sr_forward(params):
+    """
+        Create a Shiprocket forward shipment using a dictionary of parameters
+        
+        Parameters:
+        - params: Dictionary containing all the required parameters:
+            - token: Shiprocket authentication token
+            - order_data: Sales order data
+            - length: Package length
+            - breadth: Package breadth
+            - height: Package height
+            - weight: Package weight
+            - courier_id: Selected courier ID
+            - pickup_location: Pickup location name (default: "warehouse")
+            - ewaybill_no: E-way bill number (default: None)
+            - request_pickup: Whether to request pickup (default: True)
+            - contact_person: Optional contact person details (default: None)
+        
+        Returns:
+        - Response from Shiprocket API as JSON
+    """     
+    
+    # Extract all parameters from the dictionary
+    token = params.get("token")
+    order_data = params.get("order_data")
+    length = params.get("length")
+    breadth = params.get("breadth")
+    height = params.get("height")
+    weight = params.get("weight")
+    courier_id = params.get("courier_id")
+    pickup_location = params.get("pickup_location", "warehouse")
+    ewaybill_no = params.get("ewaybill_no")
+    request_pickup = params.get("request_pickup", True)
+    contact_person = params.get("contact_person")
+    
     url = "https://apiv2.shiprocket.in/v1/external/shipments/create/forward-shipment"
+    
+    # Extract line items
     order_items = [
         {
             "name": item["name"],
@@ -43,36 +80,60 @@ def create_sr_forward(token, order_data, length, breadth, height, weight,courier
         } 
         for item in order_data.get("line_items", [])
     ]
+
+    # Log contact person information
+    logger.debug(f"Contact Person information is: {order_data.get('contact_persons', [])}")
+    
+    # Use provided contact person or get from order data
+    if contact_person:
+        billing_name = contact_person.get("name")
+        billing_email = contact_person.get("email")
+        billing_phone = contact_person.get("phone")
+    elif order_data.get("contact_persons") and len(order_data["contact_persons"]) > 0:
+        billing_name = order_data["customer_name"]
+        billing_email = order_data["contact_persons"][0].get("email", "")
+        billing_phone = order_data["contact_persons"][0].get("phone", "")
+    else:
+        billing_name = order_data["customer_name"]
+        billing_email = ""
+        billing_phone = ""
+
+    # Create payload
     payload = json.dumps({
-    "order_id": order_data["salesorder_number"],
-    "order_date": order_data["date"],
-    "billing_customer_name": order_data["customer_name"],
-    "billing_address": order_data["billing_address"][0]["address"],
-    "billing_city": order_data["billing_address"][0]["city"],
-    "billing_pincode": order_data["billing_address"][0]["zip"],
-    "billing_state": order_data["billing_address"][0]["state"],
-    "billing_country": order_data["billing_address"][0]["country"],
-    "billing_email": order_data["contact_persons"][0]["email"],
-    "billing_phone": order_data["contact_persons"][0]["phone"],
-    "shipping_is_billing": True,
-    "order_items": order_items,
-    "payment_method": "Prepaid",
-    "sub_total": order_data["total"],
-    "length": length,
-    "breadth": breadth,
-    "height": height,
-    "weight": weight,
-    "pickup_location": pickup_location,
-    "print_label": False,
-    "generate_manifest": False,
-    "courier_id": courier_id,
-    "ewaybill_no": ewaybill_no,
-    "request_pickup": request_pickup
+        "order_id": order_data["salesorder_number"],
+        "order_date": order_data["date"],
+        "billing_customer_name": billing_name,
+        "billing_address": order_data["billing_address"]["address"],
+        "billing_city": order_data["billing_address"]["city"],
+        "billing_pincode": order_data["billing_address"]["zip"],
+        "billing_state": order_data["billing_address"]["state"],
+        "billing_country": order_data["billing_address"]["country"],
+        "billing_email": billing_email,
+        "billing_phone": billing_phone,
+        "shipping_is_billing": True,
+        "order_items": order_items,
+        "payment_method": "Prepaid",
+        "sub_total": order_data["total"],
+        "length": length,
+        "breadth": breadth,
+        "height": height,
+        "weight": weight,
+        "pickup_location": pickup_location,
+        "print_label": False,
+        "generate_manifest": False,
+        "courier_id": courier_id,
+        "ewaybill_no": ewaybill_no,
+        "request_pickup": request_pickup
     })
+    
     headers = {
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {token}'
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
     }
+    parsed_data = json.loads(payload)
+    for key,value in parsed_data.items():
+        logger.debug(f"key is {key} and value is {value}")
+    
     response = requests.request("POST", url, headers=headers, data=payload)
     return response.json()
 
